28f25abbca1a7f6ee4e16ee8148d5620c2a7fde5
karson_k09 <karson.richardson@my.simpson.edu>
2019-09-17 12:58:15 -0500

    Added more of my assets.

--- Assets/Scripts/Karson/KarsonPlayerController.cs
+++ Assets/Scripts/Karson/KarsonPlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour
+public class KarsonPlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     private int score;
