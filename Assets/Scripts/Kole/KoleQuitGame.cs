afed8b8fb6fa2260b2dfe33ba27c8c3c028f65e0
KHicok <kolehicok@gmail.com>
2019-09-17 12:32:51 -0500

    Adding renamed files

---
+++ Assets/Scripts/Kole/KoleQuitGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KoleQuitGame : MonoBehaviour
+{
+    public void doQuitGame()
+    {
+        Debug.Log ("Exiting game.");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Kole/KoleRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KoleRotator : MonoBehaviour
+{
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime);
+    }
+}
