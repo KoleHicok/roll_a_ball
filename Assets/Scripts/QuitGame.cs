5cdc44ea5fbddf6a4e27a571eda59b56281d71da
KHicok <kolehicok@gmail.com>
2019-09-17 11:57:16 -0500

    Initial check-in of UnityCode

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void doQuitGame()
+    {
+        Debug.Log ("Exiting game.");
+        Application.Quit();
+    }
+}
