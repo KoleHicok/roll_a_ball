0f073613a1fc90d122e46fe9a71372c711bf5efe
GeoSau <saucedogeorge9@gmail.com>
2019-09-19 12:25:25 -0500

    Got My Game working

---
+++ Assets/Scripts/George/GeorgeCameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeorgeCameraController : MonoBehaviour {
+
+    public GameObject PlayerG;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - PlayerG.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = PlayerG.transform.position + offset;
+    }
+
+}
